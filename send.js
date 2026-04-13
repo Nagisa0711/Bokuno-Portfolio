cbe832a1095a95105d60032d450e5aa369dc8992
Nagisa0711 <2007taisei1107@gmail.com>
2026-04-14 02:26:20 +0900

    Add initial HTML structure to send.js

--- send.js
+++ send.js
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>認証後の送信ページ</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  ...
+</body>
+</html>
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import {
   getAuth,
